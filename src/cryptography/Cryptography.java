6a55d9fcb7a39c1eadc5504a2cc15d7edeb90a05
Matthew Belgre <61681844+Chimey33@users.noreply.github.com>
2020-03-08 20:59:36 +1100

    Update Cryptography.java

--- src/cryptography/Cryptography.java
+++ src/cryptography/Cryptography.java
@@ -33,22 +33,8 @@ public class Cryptography {
      */
 
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
-//
- RSA test = new RSA();
- test.generateKeys(2500);
-    String message = "Experienced German teachers prepared easyExperienced German teachers prepared easy articles and simple conversations in German for beginners (level A1 and A2) and intermediates (level B1 and B2) to evaluate your comprehension and leave you feeling challenged and satisfied. Just click, read, and then answer the multiple-choice questions of the associated test. Your answers get evaluated immediately, and you are ready to move on to the next exercise. It's easy, enjoyable and free.";
-        //System.out.println(test.encrypt(message));
-        BigInteger cipher = test.encryptMessage(message);
-        System.out.println(test.decryptMessage(cipher));
 
-    //String cipher = test.encrypt(message);
-   
-
-
-//new CryptographyGUI().setVisible(true);;
-
-
-
+        new CryptographyGUI().setVisible(true);;
 
     }
     
